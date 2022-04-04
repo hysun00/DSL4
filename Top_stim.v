368e20d903c71e7c1f4e8c711bad15f912aa1183
hysun00 <85315714+hysun00@users.noreply.github.com>
2022-04-04 19:33:19 +0100

    Update Top_stim.v

--- Top_stim.v
+++ Top_stim.v
@@ -26,29 +26,47 @@ parameter CLOCK_PERIOD = 10;//100MHZ
 
 reg CLK;
 reg RESET;
+reg[3:0] SW;
+
+wire CLK_MOUSE;
+wire DATA_MOUSE;
+
 wire IR_LED;
 wire [7:0] DIGIT;
 wire [3:0] SEL;
+wire[15:0] LEDS;
+wire VGA_HS;
+wire VGA_VS;
+wire [7:0] VGA_COLOUR;
     
-TopLevel
-DUT_TopLevel
+
+TOP uut
 (
     .CLK(CLK),
     .RESET(RESET),
     .IR_LED(IR_LED),
     .DIGIT(DIGIT),
-    .SEL(SEL)
+    .SEL(SEL),
+    .SW(SW),
+    .LEDS(LEDS),
+    .VGA_HS(VGA_HS),
+    .VGA_VS(VGA_VS),
+    .VGA_COLOUR(VGA_COLOUR)
 );
 
 initial begin
     RESET=1'b0;
     #10 RESET = 1'b1;
-#30 RESET =1'b0;
+    #30 RESET = 1'b0;
 end
 
 initial begin
    CLK=1'b0;
    forever #(CLOCK_PERIOD/2) CLK = ~CLK;
+end
+
+initial begin
+    SW = 4'b1100;
 end
 
 endmodule
